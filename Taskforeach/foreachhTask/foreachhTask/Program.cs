cbbc78ae90c5285ae13bbf36bbf27ef85567e6a3
fatima-28 <2n6gddb@code.edu.az>
2022-04-06 12:08:51 +0400

    tasks added

---
+++ Taskforeach/foreachhTask/foreachhTask/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace TypeForeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("lord of rings", 5);
+            Book book2 = new Book("game of thrones", 12);
+            book1.Method();
+            book2.Method();
+        }
+    }
+    public class Book
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        Book[] books = new Book[0];
+        public Book(string name, int price)
+        {
+            Name = name;
+            Price = price;
+        }
+        public void Method()
+        {
+            Book[] array = books;
+            Array.Resize(ref books, books.Length + 1);
+            books[books.Length - 1] = new Book();
+            foreach (Book item in books)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+}
